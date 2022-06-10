0783c44259a5a9e12f3391963429a8f795ad0d8b
AndhikaK <andhika.smandading@gmail.com>
2022-06-10 11:12:58 +0700

    add responsive style

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function openProduct(evt, productName) {
 }
 document.getElementById("defaultOpen").click();
 
-// section
+// section sliders
 $(".section-5 .testimonials").slick({
   infinite: true,
   slidesToShow: 3,
@@ -22,7 +22,47 @@ $(".section-5 .testimonials").slick({
   arrows: true,
   prevArrow: $(".section-5 .prev"),
   nextArrow: $(".section-5 .next"),
+  responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 });
+
+let productBreakPointOptions = [
+  {
+    breakpoint: 780,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 1,
+    },
+  },
+  {
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 1,
+    },
+  },
+  {
+    breakpoint: 400,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    },
+  },
+];
 
 $(".section-3 #chair .tab-product").slick({
   infinite: true,
@@ -31,6 +71,7 @@ $(".section-3 #chair .tab-product").slick({
   arrows: true,
   prevArrow: $(".section-3 #chair .prev"),
   nextArrow: $(".section-3 #chair .next"),
+  responsive: productBreakPointOptions,
 });
 $(".section-3 #beds .tab-product").slick({
   infinite: true,
@@ -55,4 +96,14 @@ $(".section-3 #lamp .tab-product").slick({
   arrows: true,
   prevArrow: $(".section-3 #lamp .prev"),
   nextArrow: $(".section-3 #lamp .next"),
+});
+
+// detect scroll to change navbar
+$(window).scroll(function (event) {
+  var scroll = $(window).scrollTop();
+  if (scroll > 700) {
+    $(".navbar").addClass("scrolled");
+  } else {
+    $(".navbar").removeClass("scrolled");
+  }
 });
