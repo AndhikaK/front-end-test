3ef3acae004acf95f73b4281c00a830d9543363c
AndhikaK <andhika.smandading@gmail.com>
2022-06-10 10:16:37 +0700

    implement slider in section 3

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// tab functionality
 function openProduct(evt, productName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
@@ -13,11 +14,45 @@ function openProduct(evt, productName) {
 }
 document.getElementById("defaultOpen").click();
 
-$(".testimonials").slick({
+// section
+$(".section-5 .testimonials").slick({
   infinite: true,
   slidesToShow: 3,
   slidesToScroll: 1,
   arrows: true,
-  prevArrow: $(".prev"),
-  nextArrow: $(".next"),
+  prevArrow: $(".section-5 .prev"),
+  nextArrow: $(".section-5 .next"),
+});
+
+$(".section-3 #chair .tab-product").slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows: true,
+  prevArrow: $(".section-3 #chair .prev"),
+  nextArrow: $(".section-3 #chair .next"),
+});
+$(".section-3 #beds .tab-product").slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows: true,
+  prevArrow: $(".section-3 #beds .prev"),
+  nextArrow: $(".section-3 #beds .next"),
+});
+$(".section-3 #sofa .tab-product").slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows: true,
+  prevArrow: $(".section-3 #sofa .prev"),
+  nextArrow: $(".section-3 #sofa .next"),
+});
+$(".section-3 #lamp .tab-product").slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows: true,
+  prevArrow: $(".section-3 #lamp .prev"),
+  nextArrow: $(".section-3 #lamp .next"),
 });
